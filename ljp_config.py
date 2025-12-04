21280c1a151877c873f49682e476f22ad54b5d04
puxinyxy <497225829@qq.com>
2025-12-04 10:32:52 +0800

    代码修改提交测试

--- ljp_config.py
+++ ljp_config.py
@@ -13,6 +13,7 @@ EMBEDDING_BASE_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
 EMBEDDING_API_KEY = "sk-d103be2645ca438d91892867a65cfd2c"
 
 # LLM for the agents (can still be overridden via CLI)
+# LLM候选
 # LLM_MODEL = "qwen3-max"
 LLM_MODEL = "qwen3-32b"
 LLM_BASE_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
